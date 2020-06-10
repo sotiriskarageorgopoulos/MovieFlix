b2d83f2ceec630cd14ea2fe0949b8edd12dacb61
Sotiris <sotiris@sotiriskarageorgopoulos@gmail.com>
2020-06-10 16:13:40 +0300

    All templates are ok! v.0.1

--- js/index.js
+++ js/index.js
@@ -22,3 +22,43 @@ const checkOneOnly = () => {
         }
     }
 }
+
+const createInputs = () => {
+    let divInputActors = document.getElementById("inputActors");
+    let numOfActors = parseInt(document.getElementById("numOfActors").value);
+
+    divInputActors.innerHTML = "";
+
+    for (let i = 0; i < numOfActors; i++) {
+        let labelName = document.createElement("label");
+        labelName.innerText = "Όνομα*";
+        labelName.for = "name" + i;
+        let inputName = document.createElement("input");
+        inputName.type = "text";
+        inputName.required = true;
+        inputName.id = "name" + i
+        labelName.appendChild(inputName);
+        divInputActors.appendChild(labelName);
+
+        let labelSurname = document.createElement("label");
+        labelSurname.innerText = "Επώνυμο*";
+        labelSurname.for = "surname" + i;
+        let inputSurname = document.createElement("input");
+        inputSurname.type = "text";
+        inputSurname.required = true;
+        inputSurname.id = "surname" + i
+        labelSurname.appendChild(inputSurname);
+        divInputActors.appendChild(labelSurname);
+    }
+
+    divInputActors.style.display = "flex";
+    divInputActors.style.flexFlow = "column nowrap";
+}
+
+const logOut = () => {
+    window.location.href = "./login.html";
+}
+
+const register = () => {
+    window.location.href = "./register.html";
+}
